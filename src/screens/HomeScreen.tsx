8ccadbe256fa1d60fd76f9e2504431e27797fd60
hariirawan <hari31121993@gmail.com>
2023-10-14 02:24:04 +0800

    feat: authentication beometry

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,50 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import * as Keychain from 'react-native-keychain';
+import {useNavigation} from '@react-navigation/native';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  async function load() {
+    const username = 'hariirawan';
+    const password = 'mantap123';
+
+    await Keychain.setGenericPassword(username, password, {
+      accessControl:
+        Keychain.ACCESS_CONTROL.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE,
+      securityLevel: Keychain.SECURITY_LEVEL.SECURE_SOFTWARE,
+      storage: Keychain.STORAGE_TYPE.RSA,
+    });
+
+    try {
+      const credentials = await Keychain.getGenericPassword({});
+      if (credentials) {
+        navigation.navigate('Notes');
+      } else {
+        console.log('no credential');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: '#20274A',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <TouchableOpacity
+        style={{backgroundColor: '#7F92F9', padding: 10, borderRadius: 10}}>
+        <Text>Authentication</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
